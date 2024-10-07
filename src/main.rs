315a675a0b40c4e6489404bf7d5fb1a15f55f9e9
QuoteQuack <118599418+quotequack@users.noreply.github.com>
2024-10-07 16:32:48 +0300

    Updated File so it works

--- src/main.rs
+++ src/main.rs
@@ -13,27 +13,24 @@ fn main() -> io::Result<()> {
         println!("Error: {:?}", error);
     }
 
-
-    let mut file = File::create("~/Coding/Rust/keylog/output.txt")?;
-    let input_borrow = input.borrow();
-    file.write_all(input_borrow.as_bytes())?;
-
     println!("Your text has been written to output.txt");
 
     Ok(())
 }fn callback(event: Event, string: &Rc<RefCell<String>>) {
     if event.event_type == EventType::KeyPress(Key::F4) {
         println!("Exiting...");
+        let input_borrow = string.borrow();
+        let mut file = File::create("/home/quote/Coding/Rust/keylog/output.txt").expect("Failed to create file");
+        file.write_all(input_borrow.as_bytes()).expect("Failed to write to file");
         std::process::exit(0);
     }
     let eventtype: String = match event.event_type {
         EventType::KeyPress(key) => format!("{:?}", key),
         EventType::KeyRelease(key) => format!("{:?}", key),
-        EventType::MouseMove {x, y} => format!("MouseMove({},{})", x, y),
+        EventType::MouseMove {x, y} => format!("MouseMove({},{})\n", x, y),
         EventType::ButtonPress(button) => format!("Mouse button pressed: {:?}\n", button),
         EventType::ButtonRelease(button) => format!("Mouse button released: {:?}\n", button),
         EventType::Wheel { delta_x, delta_y } => format!("Mouse wheel scrolled: ({}, {})\n", delta_x, delta_y),
-        _ => format!("Unknown event type: {:?}\n", event.event_type),
     };
     let mut input_push = string.borrow_mut();
     input_push.push_str(&eventtype);
